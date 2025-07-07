1b1bdf29e24703e366edc5868690707ce80b6268
syedislamuddin <70541292+syedislamuddin@users.noreply.github.com>
2025-07-07 17:59:34 -0400

    Create python_op.py

---
+++ airflow/dags/python_op.py
@@ -0,0 +1,57 @@
+try:
+
+    from datetime import timedelta
+    from airflow import DAG
+    from airflow.operators.python_operator import PythonOperator
+    from datetime import datetime
+    import pandas as pd
+
+    print("All Dag modules are ok ......")
+except Exception as e:
+    print("Error  {} ".format(e))
+
+
+def first_function_execute(**context):
+    print("first_function_execute   ")
+    context['ti'].xcom_push(key='mykey', value="Starting with Hello ")
+
+
+def second_function_execute(**context):
+    instance = context.get("ti").xcom_pull(key="mykey")
+    data = [{"name":"Annonymous 1","title":"Genomics"}, { "name":"Annonymous 2","title":"Proteomics"},]
+    df = pd.DataFrame(data=data)
+    print('@'*66)
+    print(df.head())
+    print('@'*66)
+
+    print("Now from followup :{} from Function 1  ".format(instance))
+
+
+with DAG(
+        dag_id="example_dag",
+        # schedule_interval="@daily",
+        schedule_interval="*/5 * * * *",  # Every 5 minutes
+        default_args={
+            "owner": "airflow",
+            "retries": 1,
+            "retry_delay": timedelta(minutes=5),
+            "start_date": datetime(2025, 7, 1),
+        },
+        catchup=True) as f:
+
+    first_function_execute = PythonOperator(
+        task_id="first_function_execute",
+        python_callable=first_function_execute,
+        provide_context=True,
+        op_kwargs={"name":"Hello Genotools"}
+    )
+
+    second_function_execute = PythonOperator(
+        task_id="second_function_execute",
+        python_callable=second_function_execute,
+        provide_context=True,
+    )
+
+first_function_execute >> second_function_execute
+
+
