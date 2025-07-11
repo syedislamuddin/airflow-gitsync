5027b14f7c3cb3dd5ce9e5795bb7aba64e04c307
syedislamuddin <70541292+syedislamuddin@users.noreply.github.com>
2025-07-11 11:02:00 -0400

    Create with-script.py

---
+++ airflow/dags/with-script.py
@@ -0,0 +1,23 @@
+from airflow import DAG
+from datetime import datetime, timedelta
+from airflow.operators.bash import BashOperator
+# from airflow.operators.docker_operator import DockerOperator
+
+
+default_args = {
+    'owner'                 : 'k8s',
+    'description'           : 'Use of the DockerOperator',
+    'depend_on_past'        : False,
+    'start_date'            : datetime(2025, 7, 6),
+    'email_on_failure'      : False,
+    'email_on_retry'        : False,
+    'retries'               : 1,
+    'retry_delay'           : timedelta(minutes=5)
+}
+
+with DAG('bash_dag_sample', default_args=default_args, schedule="* * * * *", catchup=True) as dag:
+  extract_data = BashOperator(
+      task_id="extract_data",
+      bash_command="script.sh",
+  )
+extract_data
